ee3429dd8eca58334f5fc3d1e4a2fb162ad19c4b
Mixm33 <maxime.bertin1@epsi.fr>
2020-03-20 16:44:45 +0100

    Update header.php
    ajout d'une div pour le titre

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <header style="background-color: #DEDEDE; padding: 15px; position: absolute; top: 0;left: 0; right: 0">
     <a href="index.php">Home</a>
     <a href="shop.php" style="margin-left: 25px">Shop</a>
-    <h1>The Shop</h1>
+    <div class="mid" style="text-align: center"><h1>The Shop</h1></div>
+
 </header>
