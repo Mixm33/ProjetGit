a43b00a65a6580748fad0d9c3b2414bfd7ca7f51
Mixm33 <maxime.bertin1@epsi.fr>
2020-03-20 16:01:38 +0100

    Headers-Footers
    ajout de l'headers et du footers

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,3 @@
-<p>Property of the government of Fireflies</p>
+<footer style="background-color: #DEDEDE; padding: 15px; text-align: center; position: absolute; bottom: 0; left: 0; right: 0">
+    <a href="contact.php">Contact</a>
+</footer>
--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,5 @@
-<h1>The Shop</h1>
+<header style="background-color: #DEDEDE; padding: 15px; position: absolute; top: 0;left: 0; right: 0">
+    <a href="index.php">Home</a>
+    <a href="shop.php" style="margin-left: 25px">Shop</a>
+    <h1>The Shop</h1>
+</header>
